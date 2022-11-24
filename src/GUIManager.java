9e5f105820af6574ee8ca869bdef35b875ea5a93
VinciDa33 <marcusbthomsen@gmail.com>
2022-11-24 09:09:24 +0100

    Transition testing

--- src/GUIManager.java
+++ src/GUIManager.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
--- src/InfoRoom.java
+++ src/InfoRoom.java
@@ -1,18 +1,21 @@
+import javafx.animation.FillTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
-
-import javax.sound.sampled.Line;
-
+import javafx.util.Duration;
 public class InfoRoom extends Room{
     String description;
 
@@ -28,11 +31,27 @@ public class InfoRoom extends Room{
     @Override
     public Scene createGUI() {
         //Setting up the main boxes for holding components
-        HBox horBox = new HBox();
+        Group root = new Group();
+
+        //Animation stuff
+        Rectangle fadeRect = new Rectangle(0, 0, GUIManager.getSizeX(), GUIManager.getSizeY());
+        fadeRect.setFill(Color.rgb(20, 20, 20));
+
+        FillTransition fadeInAnim = new FillTransition();
+        fadeInAnim.setDuration(Duration.millis(1000));
+        fadeInAnim.setToValue(Color.rgb(20, 20, 20, 0));
+        fadeInAnim.setShape(fadeRect);
+        fadeInAnim.play();
+
+        FillTransition fadeOutAnim = new FillTransition();
+        fadeOutAnim.setDuration(Duration.millis(1000));
+        fadeOutAnim.setToValue(Color.rgb(20, 20, 20, 1));
+        fadeOutAnim.setShape(fadeRect);
 
         VBox leftBox = new VBox();
         leftBox.setBackground(new Background(new BackgroundFill(Color.rgb(30, 30, 30), CornerRadii.EMPTY, Insets.EMPTY)));
-        leftBox.setPrefWidth(GUIManager.getSizeX()/4f*3);
+        leftBox.setPrefSize(GUIManager.getSizeX()/4f*3, GUIManager.getSizeY());
+        leftBox.setLayoutX(0);
         leftBox.setAlignment(Pos.TOP_CENTER);
 
         if (imageString != null) {
@@ -44,7 +63,8 @@ public class InfoRoom extends Room{
 
         VBox rightBox = new VBox();
         rightBox.setBackground(new Background(new BackgroundFill(Color.rgb(50, 50, 50), CornerRadii.EMPTY, Insets.EMPTY)));
-        rightBox.setPrefWidth(GUIManager.getSizeX()/4f);
+        rightBox.setPrefSize(GUIManager.getSizeX()/4f, GUIManager.getSizeY());
+        rightBox.setLayoutX(GUIManager.getSizeX()/4f*3);
         rightBox.setAlignment(Pos.CENTER);
         rightBox.setSpacing(10);
 
@@ -65,7 +85,12 @@ public class InfoRoom extends Room{
             button.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent actionEvent) {
-                    GameManager.getInstance().goToRoom(exits.get(key));
+                    fadeOutAnim.play();
+                    Timeline exitTimer = new Timeline(
+                            new KeyFrame(fadeOutAnim.getDuration(), event -> GameManager.getInstance().goToRoom(exits.get(key)))
+                    );
+                    exitTimer.play();
+                    //GameManager.getInstance().goToRoom(exits.get(key));
                 }
             });
             button.setPrefSize(240, 80);
@@ -87,9 +112,17 @@ public class InfoRoom extends Room{
 
 
         //Adding components to main component and returning scene
-        horBox.getChildren().add(leftBox);
-        horBox.getChildren().add(rightBox);
+        root.getChildren().add(leftBox);
+        root.getChildren().add(rightBox);
 
-        return new Scene(horBox, GUIManager.getSizeX(), GUIManager.getSizeY());
+
+        Timeline fade = new Timeline(
+            new KeyFrame(fadeInAnim.getDuration(), event -> fadeRect.setDisable(true))
+        );
+        fade.play();
+
+        root.getChildren().add(fadeRect);
+
+        return new Scene(root, GUIManager.getSizeX(), GUIManager.getSizeY());
     }
 }
